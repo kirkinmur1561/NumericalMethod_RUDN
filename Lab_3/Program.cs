c640e85e6016255bc14ac1ec68ce92051c407324
Кирилл Муратов <1032142155@pfur.ru>
2022-10-14 21:27:26 +0300

    append lab_3

---
+++ Lab_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        private static double BasicFunc(double x) =>
+            -Math.Pow(x, 3) - 2;
+        
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
