228bf623704ac27025c132298a020e9977752e2f
muratovka-1 <muratov.ka@1561.ru>
2022-10-31 09:40:35 +0300

    Fix lab_4

--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -10,19 +10,52 @@ namespace Lab_4
     class Program
     {
         private const decimal H = 0.01m;
+        private const int DIGITS = 5;
+       
         
+        // y(x) = x + e^(-x) - e^(-1)
         private static decimal BasicFunc(decimal x) =>
-            Math.Round(x + (decimal)Math.Pow(Math.E, (double)-x) - (decimal)Math.Pow(Math.E, -1), 5);
+            Math.Round(x + (decimal)Math.Pow(Math.E, (double)-x) - (decimal)Math.Pow(Math.E, -1), DIGITS);
 
+        /// <summary>
+        ///  Массив Xi от 0 до 100 с шагом H
+        /// </summary>
         private static decimal[] Xs =
             Enumerable.Range(0, 101)
-                .Select(s => Math.Round(s * H, 2))
+                .Select(s => Math.Round(s * H, DIGITS))
                 .ToArray();
         
+        /// <summary>
+        /// Массив Yi задается функ BasicFunc
+        /// </summary>
         private static decimal[] Ys =
             Xs
                 .Select(BasicFunc)
                 .ToArray();
+
+        // private static decimal DerivativeOrder(int order, decimal x) =>
+        //     order switch
+        //     {
+        //         0 => BasicFunc(x),
+        //         1 => Math.Round(1m - (decimal)Math.Pow(Math.E, (double)-x), 5),
+        //         2 => Math.Round((decimal)Math.Pow(Math.E, (double)-x), 5),
+        //         3 => -Math.Round((decimal)Math.Pow(Math.E, (double)-x), 5),
+        //         4 => Math.Round((decimal)Math.Pow(Math.E, (double)-x), 5)
+        //     };
+        //
+        // private static decimal F1(bool IsNextI, int index) =>
+        //     IsNextI
+        //         ? Math.Round(Ys[index] + H * DerivativeOrder(1, Xs[index]) +
+        //                      ((H * H) / 2) * DerivativeOrder(2, Xs[index]) +
+        //                      ((H * H * H) / 6) * DerivativeOrder(3, Xs[index]) +
+        //                      ((H * H * H * H) / 24) * DerivativeOrder(4, Xs[index] + H), 5)
+        //         : Math.Round(Ys[index] - H * DerivativeOrder(1, Xs[index]) +
+        //                      ((H * H) / 2) * DerivativeOrder(2, Xs[index]) -
+        //                      ((H * H * H) / 6) * DerivativeOrder(3, Xs[index]) +
+        //                      ((H * H * H * H) / 24) * DerivativeOrder(4, Xs[index] - H), 5);
+        
+            
+        
         
         /// <summary>
         /// Функция записывающие в файлы данные формата json и csv. await Task.Delay(30) предназначена для гаранта закрытия потока на запись.
@@ -31,12 +64,15 @@ namespace Lab_4
         /// <param name="pointDs">Массив координат</param>
         private static async Task WriteFiles(string namePoint, PointD[] pointDs)
         {
-            int num = new Random().Next(0, 1001);
-            await File.WriteAllTextAsync($"{num}_{namePoint}_file.json",
+            string date = DateTime.Now.ToString("G")
+                .Replace(":", "")
+                .Replace("/", "");
+            
+            await File.WriteAllTextAsync($"{date}_{namePoint}_file.json",
                 JsonSerializer.Serialize(pointDs), Encoding.UTF8);            
             await Task.Delay(30);
 
-            await File.WriteAllTextAsync($"{num}_{namePoint}_file.csv", 
+            await File.WriteAllTextAsync($"{date}_{namePoint}_file.csv", 
                 string.Join("\n", pointDs.Select(s => $"{s.X};{s.Y}")));
             
             await Task.Delay(30);
@@ -45,10 +81,14 @@ namespace Lab_4
         
         static async Task Main(string[] args)
         {
-            await WriteFiles("Origin", Enumerable
-                .Range(0, Xs.Length)
-                .Select(index => new PointD(Xs[index], Ys[index]))
-                .ToArray());
+            // await WriteFiles("Origin", Enumerable
+            //     .Range(0, Xs.Length)
+            //     .Select(index => new PointD(Xs[index], Ys[index]))
+            //     .ToArray());
+
+            
+
+           
         }
     }
 }
